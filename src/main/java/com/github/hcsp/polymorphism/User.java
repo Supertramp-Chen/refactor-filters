759a59ea2e86d5e31fa6b93fe804d8f5e0b116b7
Chen <mforward@126.com>
2022-09-09 20:59:46 +0800

    把一个条件当做参数提取出来 java8之前只能通过接口+类的方法实现一个抽象的操作 函数式操作 	没有一个好的方法去表示一个抽象的操作 只能使用一个对象存储一些数据 当做函数的返回值

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -5,10 +5,14 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class User {
-    /** 用户ID，数据库主键，全局唯一 */
+    /**
+     * 用户ID，数据库主键，全局唯一
+     */
     private final Integer id;
 
-    /** 用户名 */
+    /**
+     * 用户名
+     */
     private final String name;
 
     public User(Integer id, String name) {
@@ -26,38 +30,54 @@ public class User {
 
     // 过滤ID为偶数的用户
     public static List<User> filterUsersWithEvenId(List<User> users) {
-        List<User> results = new ArrayList<>();
-        for (User user : users) {
-            if (user.id % 2 == 0) {
-                results.add(user);
-            }
-        }
-        return results;
+        return filter(users, new isUsersWithEvenId());
     }
 
     // 过滤姓张的用户
     public static List<User> filterZhangUsers(List<User> users) {
-        List<User> results = new ArrayList<>();
-        for (User user : users) {
-            if (user.name.startsWith("张")) {
-                results.add(user);
-            }
-        }
-        return results;
+        return filter(users, new isZhangUsers());
     }
 
     // 过滤姓王的用户
     public static List<User> filterWangUsers(List<User> users) {
+        return filter(users, new isWangUsers());
+    }
+    // 你可以发现，在上面三个函数中包含大量的重复代码。
+    // 请尝试通过Predicate接口将上述代码抽取成一个公用的过滤器函数
+    // 并简化上面三个函数
+
+
+    /*如果参数是个条件
+     * java8之前只能通过接口+类的方法实现一个抽象的操作 函数式操作
+     * 把抽象的条件提取出来*/
+    static class isWangUsers implements Predicate<User> {
+        @Override
+        public boolean test(User user) {
+            return user.name.startsWith("王");
+        }
+    }
+
+    static class isZhangUsers implements Predicate<User> {
+        @Override
+        public boolean test(User user) {
+            return user.name.startsWith("张");
+        }
+    }
+
+    static class isUsersWithEvenId implements Predicate<User> {
+        @Override
+        public boolean test(User user) {
+            return user.name.startsWith("张");
+        }
+    }
+
+    public static List<User> filter(List<User> users, Predicate<User> predicate) {
         List<User> results = new ArrayList<>();
         for (User user : users) {
-            if (user.name.startsWith("王")) {
+            if (predicate.test(user)) {
                 results.add(user);
             }
         }
         return results;
     }
-    // 你可以发现，在上面三个函数中包含大量的重复代码。
-    // 请尝试通过Predicate接口将上述代码抽取成一个公用的过滤器函数
-    // 并简化上面三个函数
-    public static List<User> filter(List<User> users, Predicate<User> predicate) {}
 }
